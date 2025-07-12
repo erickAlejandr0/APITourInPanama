7de2f5600592251e8b261b99fc82071dcfdba0af
erick <e.alejandro1705@gmail.com>
2025-07-12 17:53:28 -0500

    ajustes en los modelos de salida

--- app/Models/actividadesModel_out.py
+++ app/Models/actividadesModel_out.py
@@ -2,21 +2,26 @@ from pydantic import BaseModel
 from typing import Optional
 
 class ActividadOut(BaseModel):
+    id: int
     encabezado: str
     descp: str
     rating: Optional[float]
     latitud: float
     longitud: float
     foto_url: Optional[str]
-
-class ActividadesCategoriaOut(BaseModel):
     id_cat: int
+
+class ActividadCercanaOut(BaseModel):
+    id: int
     encabezado: str
     descp: str
     rating: Optional[float]
     latitud: float
     longitud: float
+    distancia_m: float
     foto_url: Optional[str]
+    id_categoria: int
+
 
 class Config:
     orm_mode = True
