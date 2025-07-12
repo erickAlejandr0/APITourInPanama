47fe846e45c559a6d24029581497fbb985dd01f1
erick <e.alejandro1705@gmail.com>
2025-07-12 15:39:32 -0500

    se añadio una nueva ruta para traer las actividades segun su categoria y se añadio un modelo OUT para estas mismas

--- app/Models/actividadesModel_out.py
+++ app/Models/actividadesModel_out.py
@@ -9,5 +9,14 @@ class ActividadOut(BaseModel):
     longitud: float
     foto_url: Optional[str]
 
+class ActividadesCategoriaOut(BaseModel):
+    id_cat: str
+    encabezado: str
+    descp: str
+    rating: Optional[float]
+    latitud: float
+    longitud: float
+    foto_url: Optional[str]
+
 class Config:
     orm_mode = True
--- app/routers/actividades.py
+++ app/routers/actividades.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Request
 import asyncpg
-from app.Models.actividadesModel_out import ActividadOut
+from app.Models.actividadesModel_out import ActividadOut,ActividadesCategoriaOut
 from app.dataBase.db import connect_db
 
 router = APIRouter(
@@ -23,4 +23,18 @@ async def get_actividad():
     except Exception as e:
         raise HTTPException(500,f"error al cargar actividades desde la base de datos{str(e)}")
 
+@router.get("/{idcategoria}", response_model=list[ActividadesCategoriaOut])
+async def get_actividad(idcategoria: int):
+    try:
+        conn = await connect_db()
+        result = await conn.fetch(
+            "SELECT * FROM actividades_por_categoria($1)", idcategoria
+        )
+        await conn.close()
+        if result: 
+            return[dict(row) for row in result]
+        
+        return[]
+    except Exception as e:
+        raise HTTPException(500,f"error al cargar actividades desde la base de datos{str(e)}")
         
