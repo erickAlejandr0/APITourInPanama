c7e1da22936567f8ed924430373f27c3bde3bd8c
erick <e.alejandro1705@gmail.com>
2025-07-11 22:48:03 -0500

    cambio de variables en modelo actividades out

--- app/Models/actividadesModel_out.py
+++ app/Models/actividadesModel_out.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from typing import Optional
 
 class ActividadOut(BaseModel):
-    titulo: str
+    encabezado: str
     descripcion: str
     calificacion: Optional[float]
     latitud: float
