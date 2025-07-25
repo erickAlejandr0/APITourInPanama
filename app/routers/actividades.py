0dda57795f2a1676398bf9ff6ac8aa436491d02f
erick <e.alejandro1705@gmail.com>
2025-07-24 20:36:48 -0500

    se añadio el endpointpara la funcionalidad guardar actividad en el itinerario

--- app/routers/actividades.py
+++ app/routers/actividades.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, Request
 import asyncpg
 from typing import Union
 from app.Models.actividadesModel_out import ActividadOut,ActividadCercanaOut,MensajeOut
+from app.Models.itinerarioModel import Itinerario
 from app.dataBase.db import connect_db
 
 router = APIRouter(
@@ -56,4 +57,19 @@ async def get_actividad(lat:float, lon:float, radio:float):
         return[dict(row) for row in result]
     except Exception as e:
         raise HTTPException(500,f"error al cargar actividades cercanas: {str(e)}")
+    
+@router.post("/itinerario", response_model = MensajeOut)
+async def save_itinerario(i:Itinerario):
+    try:
+        conn = await connect_db()
+        result = await conn.fetch(
+            "SELECT * FROM guardar_actividad($1,$2,$3)",i.fecha,i.hora,i.nota
+        )
+        await conn.close()
+        if result:
+            return MensajeOut(mensaje = "actividad guardada en el itinerario")
+        else:
+            raise HTTPException(status_code=400, detail="No se guardó la actividad")
         
+    except Exception as e:
+        raise HTTPException(500,f"error al guardar el itinerario:{str(e)}")
