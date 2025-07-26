a81f4edf86436d21450f34e24794c42a4767572b
erick <e.alejandro1705@gmail.com>
2025-07-26 18:13:41 -0500

    se añadieron dependencias para manejar los datos captados del perfil

--- app/routers/perfiles.py
+++ app/routers/perfiles.py
@@ -23,25 +23,34 @@ router = APIRouter(
     tags=["perfiles"]
 )
 
+
+
+
 @router.put("/cargar/{id}", response_model=MensajeOut)
-async def cargarFoto(id: int, u: FotoPerfilDTO):
-    try:
+async def subir_foto(user_id: str = Form(...), imagen: UploadFile = File(...)):
+    # 1. Obtener ruta de imagen actual desde la BD
+    perfil = supabase.table("perfiles").select("foto").eq("id", user_id).single().execute()
+    imagen_actual = perfil.data["foto"] if perfil.data else None
 
-        conn = await connect_db()
-        result = await conn.fetchval(
-             "SELECT * FROM cargar_foto($1,$2)", id, u.foto_url
-        )
+    # 2. Si hay una imagen previa, borrarla
+    if imagen_actual:
+        nombre_archivo = imagen_actual.split("/")[-1]
+        supabase.storage.from_(BUCKET).remove([nombre_archivo])
 
-        if not result:
-            return MensajeOut(status_code=400, detail=" No se pudo guardar la foto de perfil")
-        else:
-            return result
-    except Exception as e:
-        raise HTTPException(500,f"error al guardar foto  :{str(e)}")
-    finally:
-        if conn:
-            await conn.close() 
-    
+    # 3. Subir nueva imagen al bucket
+    extension = imagen.filename.split(".")[-1]
+    nuevo_nombre = f"{uuid.uuid4()}.{extension}"
+    contenido = await imagen.read()
+
+    supabase.storage.from_(BUCKET).upload(nuevo_nombre, contenido)
+
+    # 4. Obtener URL pública
+    nueva_url = supabase.storage.from_(BUCKET).get_public_url(nuevo_nombre)
+
+    # 5. Actualizar la tabla perfiles
+    supabase.table("perfiles").update({"foto": nueva_url}).eq("id", user_id).execute()
+
+    return {"mensaje": "Imagen actualizada", "url": nueva_url}
     
 @router.get("/obtener-foto/{id}", response_model=MensajeOut)
 async def cargarFoto(id: int):
