642bb83b3791d312fac3b847f723b9b38f1067a2
erick <e.alejandro1705@gmail.com>
2025-07-26 20:33:58 -0500

    solucion de errores

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import usuarios, actividades,perfiles
+from .routers import usuarios, actividades,perfiles
 from contextlib import asynccontextmanager
 
 
