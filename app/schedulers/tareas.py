81e3ab324048ae49eea0a7a8456a5c38bf584174
erick <e.alejandro1705@gmail.com>
2025-07-29 00:18:58 -0500

    prueba de tareas programadas

--- app/schedulers/tareas.py
+++ app/schedulers/tareas.py
@@ -1,13 +1,11 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import asyncpg
-import asyncio
 from app.dataBase.db import connect_db
-
 
 async def eliminar_actividades_expiradas():
     try:
         conn = await connect_db()
-        await conn.execute("SELECT * FROM eliminar_actividades_expiradas();")
+        await conn.execute("SELECT * FROM eliminar_actividades_expiradas()")
         print("Actividades expiradas eliminadas")
     except Exception as e:
         print(f"Error al eliminar actividades expiradas: {e}")
@@ -16,7 +14,16 @@ async def eliminar_actividades_expiradas():
             await conn.close()
 
 def iniciar_scheduler():
+    print("Iniciando scheduler...")
     scheduler = AsyncIOScheduler()
     # Ejecutar cada hora
-    scheduler.add_job(eliminar_actividades_expiradas, "interval", hours=1)
+    scheduler.add_job(
+        eliminar_actividades_expiradas,
+        trigger="interval",
+        minutes=3,
+        id="eliminar_actividades_expiradas",  # opcional: para identificar el job
+        replace_existing=True  # reemplaza si ya existe un job con el mismo ID
+        
+    )
     scheduler.start()
+    print("Scheduler iniciado, itinerarios limpiados correctamente")
